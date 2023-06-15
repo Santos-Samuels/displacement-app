a44a72e40abd5b2909347db753e137b0482fc9df
Santos-Samuels <saamsmith15@gmail.com>
2023-06-15 16:28:14 -0300

    ♻️ refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 "use client";
-import DisplacementList from "@/components/displacment/DisplacementList";
-import ConductorList from "@components/conductor/ConductorList";
-import CustomerList from "@components/customer/CustomerList";
-import VehicleList from "@components/vehicle/VehicleList";
 
 export default function Home() {
   return (
     <main>
-      <CustomerList />
-      <VehicleList />
-      <ConductorList />
-      <DisplacementList />
+      <h1>here comes the dashboard</h1>
     </main>
   );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import ConductorList from "./conductor/ConductorList";
+import CustomerList from "./customer/CustomerList";
+import DisplacementList from "./displacment/DisplacementList";
+import VehicleList from "./vehicle/VehicleList";
+
+export { DisplacementList, ConductorList, CustomerList, VehicleList };
