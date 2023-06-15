22c8dff2075f77d36298a4003417034304155ea0
Santos-Samuels <saamsmith15@gmail.com>
2023-06-15 16:27:26 -0300

    ✨ add conductors page

---
+++ src/app/condutores/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { ConductorList } from "@/components"
+
+const ConductorPage = () => {
+  return (
+    <main>
+      <ConductorList />
+    </main>
+  )
+}
+
+export default ConductorPage
