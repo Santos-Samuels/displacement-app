7ff25c18707d5a3d30a68bcad1fe7407e69d3587
Santos-Samuels <saamsmith15@gmail.com>
2023-06-15 16:27:11 -0300

    ✨ add customers page

---
+++ src/app/clientes/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { CustomerList } from "@/components";
+
+const CustomerPage = () => {
+  return (
+    <main>
+      <CustomerList />
+    </main>
+  );
+}
+
+export default CustomerPage;
